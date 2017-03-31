44ffa41b66ca722847995c3426f5ecb0fd4986c0
Lejia Duan <lduan@thoughtworks.com>
2017-03-31 15:20:54 -0500

    adds gitignore and basic keypad ui with display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            let textInDisplay = display.text!
+            display.text = textInDisplay + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
 }
 
