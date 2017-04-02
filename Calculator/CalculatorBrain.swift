9b84b45225ce6b31456880e678bb29cba7899fc4
Lejia Duan <lduan@thoughtworks.com>
2017-04-01 23:21:18 -0500

    creates enum for operations and adds unary operations

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,20 +8,42 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
 class CalculatorBrain {
     
     private var accumulator: Double?
     
+    private enum Operation {
+        case constant(Double)
+        case unary((Double) -> Double)
+        case binary((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unary(sqrt),
+        "±" : Operation.unary(changeSign)
+    ]
+    
     func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unary(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binary:
+                break
+            case .equals:
+                break
             }
-        default:
-            break
         }
     }
     
