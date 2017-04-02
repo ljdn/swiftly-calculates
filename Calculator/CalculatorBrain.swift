3203bc78bc7931feb56d774d01e3a79ec4897f5e
Lejia Duan <lduan@thoughtworks.com>
2017-04-01 23:44:34 -0500

    adds binary operations

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -27,7 +27,12 @@ class CalculatorBrain {
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
         "√" : Operation.unary(sqrt),
-        "±" : Operation.unary(changeSign)
+        "±" : Operation.unary(changeSign),
+        "×" : Operation.binary(*),
+        "÷" : Operation.binary(/),
+        "+" : Operation.binary(+),
+        "-" : Operation.binary(-),
+        "=" : Operation.equals
     ]
     
     func performOperation(_ symbol: String) {
@@ -39,11 +44,32 @@ class CalculatorBrain {
                 if accumulator != nil {
                     accumulator = function(accumulator!)
                 }
-            case .binary:
-                break
+            case .binary(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
             case .equals:
-                break
+                performPendingBinaryOperation()
             }
+        }
+    }
+    
+    private func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
         }
     }
     
