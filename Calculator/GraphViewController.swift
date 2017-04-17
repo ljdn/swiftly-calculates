511b9439c0499d2c0d58fdf76b37f0186787ec80
Lejia Duan <lduan@thoughtworks.com>
2017-04-17 14:46:37 -0500

    adds new view controller and graph button

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by lduan on 4/17/17.
+//  Copyright © 2017 lduan. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+}
